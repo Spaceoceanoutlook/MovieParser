a3a551e103a4d1151dae6011a351cd929371d4f3
Spaceocean <spaceocean@outlook.com>
2024-08-30 16:55:06 +0500

    added models

---
+++ models.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import declarative_base, mapped_column, relationship
+from sqlalchemy.orm import Mapped
+from sqlalchemy import Integer, String, ForeignKey
+from typing import List
+
+# Определение базового класса
+Base = declarative_base()
+
+
+class FilmGenre(Base):
+    __tablename__ = 'film_genre'
+
+    film_id: Mapped[int] = mapped_column(ForeignKey('films.id'), primary_key=True)
+    genre_id: Mapped[int] = mapped_column(ForeignKey('genres.id'), primary_key=True)
+
+
+class Film(Base):
+    __tablename__ = 'films'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, unique=True)
+    year: Mapped[int] = mapped_column(Integer)
+    countries: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    rating: Mapped[int] = mapped_column(Integer)
+    img: Mapped[str] = mapped_column(String)
+    # Связь с жанрами
+    genres: Mapped[List["Genre"]] = relationship("Genre", secondary="film_genre", back_populates="films")
+
+
+class Genre(Base):
+    __tablename__ = 'genres'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    # Связь с фильмами
+    films: Mapped[List["Film"]] = relationship("Film", secondary="film_genre", back_populates="genres")
